2c4ef4b925f85a41dc49b6f6b86da106c64bee03
Mr Black <omarx8585@gmail.com>
2021-05-04 14:17:38 -0600

    Updated fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,23 @@
-window.alert("This is an alert! javaScript is running!");
+/* this creates a dialogue box to type inputs: var represents value 
+that gets stored in browsers memory. This var is holding our robots name*/
+
+var playerName = window.prompt("What is your robot's name?");
+
+// Note the lack of quotation marks around playerName 
+
+//What is this?
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10+10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+
+// this creates a function named "fight" always add the name and "call" the function
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight(); 
